07ee1fae363433cfda5a028bf073912bf0d33df6
Gene De Lisa <gene@rockhoppertech.com>
2019-12-24 12:41:24 -0500

    Updated to Swift 5. Added comments.

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
-#PlayerNodePlayground
+# PlayerNodePlayground
 
 
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://en.wikipedia.org/wiki/MIT_License)
 [![GitHub tag](https://img.shields.io/github/tag/genedelisa/PlayerNodePlayground.svg)](https://github.com/genedelisa/PlayerNodePlayground/)
 [![GitHub release](https://img.shields.io/github/release/genedelisa/PlayerNodePlayground.svg)](https://github.com/genedelisa/PlayerNodePlayground/)
 
-A Swift 2 Playground example using `AVAudioPlayerNode` along with effects `AVAudioUnitDelay`, `AVAudioUnitReverb`, and `AVAudioUnitTimePitch` attached to an `AVAudioEngine` to play an audio file.
+[![Swift 5](https://img.shields.io/badge/swift5-compatible-4BC51D.svg?style=flat")](https://developer.apple.com/swift)
+
+[![Sponsors](https://img.shields.io/badge/Sponsors-Rockhopper%20Technologies-orange.svg?style=flat)](http://www.rockhoppertech.com/)
+[![Twitter @GeneDeLisaDev](https://img.shields.io/twitter/follow/GeneDeLisaDev.svg?style=social)](https://twitter.com/GeneDeLisaDev)
+![GitHub followers](https://img.shields.io/github/followers/genedelisa.svg?label=Follow&style=social)
+
+## What's this?
+
+A Swift Playground example using `AVAudioPlayerNode` along with effects `AVAudioUnitDelay`, `AVAudioUnitReverb`, and `AVAudioUnitTimePitch` attached to an `AVAudioEngine` to play an audio file.
+
 
 
 ## Blog post for this example.
 
+Someday maybe.
+
 [Blog post](http://www.rockhoppertech.com/blog/)
+
+
+## Bugs
+
+
+[![GitHub issues](https://img.shields.io/github/issues/genedelisa/hms-seconds.svg)](https://github.com/genedelisa/hms-seconds/issues)
+
+If you find one, [please add it to issues](https://github.com/genedelisa/hms-seconds/issues)
 
 
 
@@ -18,7 +37,8 @@ A Swift 2 Playground example using `AVAudioPlayerNode` along with effects `AVAud
 
 [![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=F5KE9Z29MH8YQ&bnP-DonationsBF:btn_donate_SM.gif:NonHosted)
 
-<img src="http://www.rockhoppertech.com/blog/wp-content/uploads/2015/05/IMG_0657.png" alt="Giacomo Kitty" width="400" height="300">
+<img src="http://www.rockhoppertech.com/blog/wp-content/uploads/2016/07/momocoding-1024.png" alt="Giacomo Kitty" width="400" height="300">
+
 
 ## Licensing
 
@@ -28,6 +48,6 @@ Please read the [LICENSE](LICENSE) for details.
 
 ## Credits
 
-*	[Gene De Lisa's development blog](http://rockhoppertech.com/blog/)
-*	[Gene De Lisa's music blog](http://genedelisa.com/)
-*   Twitter: [@GeneDeLisaDev](http://twitter.com/genedelisadev)
+* [Gene De Lisa's development blog](http://rockhoppertech.com/blog/)
+* [Gene De Lisa's music blog](http://genedelisa.com/)
+* Twitter: [@GeneDeLisaDev](http://twitter.com/genedelisadev)
--- playernodePlayground.playground/Contents.swift
+++ playernodePlayground.playground/Contents.swift
@@ -1,22 +1,16 @@
-import UIKit
+
+/*:
+ # AVAudioPlayerNode
+ How to read a sound file, add effects, and play the damn thing.
+ */
+
 import AVFoundation
 import PlaygroundSupport
 
-guard let audioFileURL = Bundle.main.url(forResource: "snare-analog", withExtension: "wav") else {
-    fatalError("audio file is not in bundle.")
-}
-
-var audioFile: AVAudioFile?
-do {
-    audioFile = try AVAudioFile(forReading: audioFileURL)
-} catch {
-    fatalError("canot create AVAudioFile \(error)")
-}
-
-
+//: Create the angine
 let engine = AVAudioEngine()
-let player = AVAudioPlayerNode()
 
+//: Create a few effects
 let delay = AVAudioUnitDelay()
 delay.delayTime = 2
 delay.feedback = 50
@@ -31,41 +25,72 @@ var timePitch = AVAudioUnitTimePitch()
 timePitch.pitch = 100 // cents
 timePitch.rate = 2
 
+//: Finally, create the star of the show
+let player = AVAudioPlayerNode()
+
+//: Now we have 3 nodes. Attach them to the engine to use them. The order doesn't matter here.
 engine.attach(player)
 engine.attach(delay)
 engine.attach(reverb)
 engine.attach(timePitch)
 
-let format = audioFile!.processingFormat
-engine.connect(player, to: delay, format: format)
-engine.connect(delay, to: reverb, format: format)
-engine.connect(reverb, to: engine.mainMixerNode, format: format)
+//: Read the sound file. It's in the Resources folder.
 
+// mp3 seems to hang
+// guard let audioFileURL = Bundle.main.url(forResource: "jfk", withExtension: "mp3") else {
 
-let session = AVAudioSession.sharedInstance()
+// wav works
+guard let audioFileURL = Bundle.main.url(forResource: "snare-analog", withExtension: "wav") else {
+    fatalError("audio file is not in bundle.")
+}
+
+var audioFile: AVAudioFile?
 do {
-    try session.setCategory(AVAudioSessionCategoryPlayback)
-    try session.overrideOutputAudioPort(.speaker)
-    try session.setActive(true)
+    audioFile = try AVAudioFile(forReading: audioFileURL)
 } catch {
-    fatalError("cannot create/set session \(error)")
+    fatalError("canot create AVAudioFile \(error)")
 }
 
-
-do {
-    try engine.start()
-} catch {
-    fatalError("Could not start engine. error: \(error).")
+//: If this fails, you messed up. Is the sound file there?
+if let soundFile = audioFile {
+    
+    //: Now create the "graph". You can have fun here trying different connections.
+    let format = soundFile.processingFormat
+    engine.connect(player, to: delay, format: format)
+    engine.connect(delay, to: reverb, format: format)
+    engine.connect(reverb, to: engine.mainMixerNode, format: format)
+    
+    //: macOS doesn't have an audio session, so do this check.
+    #if os(iOS) || os(watchOS) || os(tvOS)
+    let session = AVAudioSession.sharedInstance()
+    do {
+        try session.setCategory(AVAudioSession.Category.playback)
+        try session.overrideOutputAudioPort(.speaker)
+        try session.setActive(true)
+    } catch {
+        fatalError("cannot create/set session \(error)")
+    }
+    #endif
+    
+    //: Start your engines!
+    do {
+        try engine.start()
+    } catch {
+        fatalError("Could not start engine. error: \(error).")
+    }
+    
+    //: Now play it! See the docs for the time thing. nil means "now".
+    let when: AVAudioTime? = nil
+    player.scheduleFile(soundFile, at: when) {
+        print("scheduled now")
+        // just for fun
+        player.rate = 0.5
+        player.play()
+    }
+    
+    //: or just play
+    // player.play()
 }
 
-
-//player.scheduleFile(audioFile!, atTime: nil, completionHandler: nil)
-player.scheduleFile(audioFile!, at: nil) {
-    print("done")
-    player.rate = 0.5
-    player.play()
-}
-
-player.play()
-
+//: you might not hear anything if you don't do this. You'll have to hit the stop button.
 PlaygroundPage.current.needsIndefiniteExecution = true
